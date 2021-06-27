c9e235a3782bc730ea6b5db0557286093ab787ef
Senne32 <mateussenne@hotmail.com>
2021-06-27 19:52:49 -0300

    declarando variaveis e pegando valores iniciais

---
+++ script.ts
@@ -0,0 +1,12 @@
+var data, hora, minuto, segundo;
+
+function init()
+{
+
+    data = new Date();
+    hora = data.getHours();
+    minuto = data.getMinutes();
+    segundo = data.getSeconds();
+
+}
+
